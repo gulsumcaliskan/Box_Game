60fa7df02609ffc7655e167b96dc952733befff9
Gülsüm ÇALIŞKAN <gulsumfb95@gmail.com>
2022-04-28 00:11:38 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 
 public class Main {
 	public static void main(String[] args){
-		Fighter f1 = new Fighter("A", 10, 120, 100, 30, 50);
-		Fighter f2 = new Fighter("B", 20, 85, 85, 40, 50);
+		Fighter f1 = new Fighter("Johnny Cage", 10, 120, 100, 30, 50);
+		Fighter f2 = new Fighter("Reiko", 20, 85, 85, 40, 50);
 		
 		Match match = new Match(f1, f2, 85, 100);
 		match.run();
